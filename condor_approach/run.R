d71dca47efbc619ef2672f297cfa6889985fec9e
Brian O'Meara <omeara.brian@gmail.com>
2019-07-15 22:46:57 -0400

    many more runs

--- condor_approach/run.R
+++ condor_approach/run.R
@@ -2,7 +2,7 @@ args <- commandArgs(trailingOnly = TRUE)
 print("args")
 print(args)
 
-condition.matrix <- expand.grid(ntax=c(15,30,100), nhybridizations=c(1,2,5), tree.height=1, sigma.sq=0.01, mu=1, bt=c(1,1.2), vh=c(0,1), SE=c(0,1), gamma=0.5, rep=sequence(5))
+condition.matrix <- expand.grid(ntax=c(30,100), nhybridizations=c(1,2,5,10), tree.height=50, sigma.sq=0.01, mu=1, bt=c(1,2), vh=c(0,0.1*50*0.01,50*0.01), SE=c(0, 0.1*sqrt(0.01*50), 0.5*sqrt(0.01*50)), gamma=0.5, rep=sequence(100)) #for Vh, zero, as much variance as you get from 10% of evo history, or as much variance as you get from the whole tree. For SE, 10% and half of the variance from BM
 
 base_number <- as.numeric(args[2]) + 1
 
@@ -23,4 +23,4 @@ DoRunTemplated <- function(ntax, nhybridizations, tree.height, sigma.sq, mu, bt,
 
 result <- DoRunTemplated(ntax=conditions$ntax, nhybridizations=conditions$nhybridizations, tree.height=conditions$tree.height, sigma.sq=conditions$sigma.sq, mu=conditions$mu, bt=conditions$bt, vh=conditions$vh, SE=conditions$SE, gamma=conditions$gamma)
 
-save(result, conditions, file=paste0("Result_Arg1_", args[1], "_arg2_", args[2], "_ntax_", conditions$ntax, "_nhybridizations_",conditions$nhybridizations, "_treeheight_",conditions$tree.height, "_sigmasq_",conditions$sigma.sq, "_mu_",conditions$mu, "_bt_", conditions$bt, "_vh_", conditions$vh, "_SE_", conditions$SE, "_gamma_", conditions$gamma, ".rda"))
+save(result, conditions, file=paste0("Result_Arg1_", args[1], "_arg2_", args[2], "_ntax_", conditions$ntax, "_nhybridizations_",conditions$nhybridizations, "_treeheight_",conditions$tree.height, "_sigmasq_",conditions$sigma.sq, "_mu_",conditions$mu, "_bt_", conditions$bt, "_vh_", conditions$vh, "_SE_", round(conditions$SE,5), "_gamma_", conditions$gamma,"_rep_", conditions$rep, ".rda"))
