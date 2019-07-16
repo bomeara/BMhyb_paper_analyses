bfead696897d20c7fe8a91fb149b1f5d19705a6b
Brian O'Meara <omeara.brian@gmail.com>
2019-07-16 15:11:46 -0400

    gathering data

--- condor_approach/gather.R
+++ condor_approach/gather.R
@@ -1,28 +1,36 @@
 library(BMhyb)
 library(dplyr)
-condition.matrix <- expand.grid(ntax=c(30,100), nhybridizations=c(1,5,10), tree.height=50, sigma.sq=0.01, mu=1, bt=c(1,2), vh=c(0,0.1*50*0.01), SE=c(0, 0.1*sqrt(0.01*50)), gamma=0.5, rep=sequence(50)) #for Vh, zero, as much variance as you get from 10% of evo history, or as much variance as you get from the whole tree. For SE, 10% and half of the variance from BM
 
 all.results <- data.frame()
 
-for (i in sequence(nrow(condition.matrix))) {
+relevant.files <- list.files(pattern="Result.*rda")
+for (file.index in seq_along(relevant.files)) {
   results <- NULL
   conditions <- NULL
-  relevant_file_string <- paste0("_ntax_", conditions$ntax, "_nhybridizations_",conditions$nhybridizations, "_treeheight_",conditions$tree.height, "_sigmasq_",conditions$sigma.sq, "_mu_",conditions$mu, "_bt_", conditions$bt, "_vh_", conditions$vh, "_SE_", round(as.numeric(conditions$SE),5), "_gamma_", conditions$gamma,"_rep_", conditions$rep, ".rda")
-  relevant_files <- list.files(pattern=relevant_file_string)
-  for (file_index in seq_along(relevant_files)) {
-    try(load(relevant_files[file_index]))
-    if(!is.null(results)) {
-      local.results <- data.frame()
-      for (result.index in seq_along(results)) {
-        result <- results[[result.index]]
-        result.params <- c(result$best, conditions)
-        local.results <- plyr::rbind.fill(local.results, result.params)
-      }
-      local.results$deltaAICc <- local.results$AICc - min(local.results$AICc)
-      rel.lik <- exp(-0.5* local.results$deltaAICc)
-      local.results$AkaikeWeight <- rel.lik / sum(rel.lik)
-      all.results <- plyr::rbind.fill(all.results, local.results)
+  try(load(relevant.files[file.index]))
+  if(!is.null(results)) {
+    names(conditions) <- paste0(names(conditions), ".true")
+    local.results <- data.frame()
+    for(i in sequence(length(results))) {
+      local.row <- cbind(results[[i]]$best, conditions)
+      local.results <- plyr::rbind.fill(local.results, local.row)
     }
+    local.results$deltaAICc <- local.results$AICc - min(local.results$AICc)
+    rel.lik <- exp(-0.5* local.results$deltaAICc)
+    local.results$AkaikeWeight <- rel.lik / sum(rel.lik)
+    all.results <- plyr::rbind.fill(all.results, local.results)
   }
 }
+all.results$SE.fixed <- FALSE
+all.results$SE.fixed[which(is.na(all.results$SE))] <- TRUE
+all.results$SE[which(is.na(all.results$SE))] <- 0
+
+all.results$bt.fixed <- FALSE
+all.results$bt.fixed[which(is.na(all.results$bt))] <- TRUE
+all.results$bt[which(is.na(all.results$bt))] <- 1
+
+all.results$vh.fixed <- FALSE
+all.results$vh.fixed[which(is.na(all.results$vh))] <- TRUE
+all.results$vh[which(is.na(all.results$vh))] <- 0
+
 save(all.results, file="Summary.rda")
