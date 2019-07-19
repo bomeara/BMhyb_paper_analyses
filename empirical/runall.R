d9f8987794f25ab6114d18eb7a2965b84f38910a
Brian O'Meara <omeara.brian@gmail.com>
2019-07-19 12:37:01 -0400

    8 cores on the imac

--- empirical/runall.R
+++ empirical/runall.R
@@ -2,10 +2,10 @@ library(BMhyb)
 
 data(cichlid)
 
-c.results.no.empirical.se <- BMhybExhaustive(phy.graph=cichlid$phy.graph, traits=cichlid$trait, confidence.points=5000)
+c.results.no.empirical.se <- BMhybExhaustive(phy.graph=cichlid$phy.graph, traits=cichlid$trait, confidence.points=5000, ncores=8)
 save(c.results.no.empirical.se, file="CichlidResultsNo.rda")
-c.results.yes.empirical.se <- BMhybExhaustive(phy.graph=cichlid$phy.graph, traits=cichlid$trait, measurement.error=cichlid$final.se, confidence.points=5000)
+c.results.yes.empirical.se <- BMhybExhaustive(phy.graph=cichlid$phy.graph, traits=cichlid$trait, measurement.error=cichlid$final.se, confidence.points=5000, ncores=8)
 save(c.results.yes.empirical.se, file="CichlidResultsYes.rda")
 data(nicotiana)
-n.results <- BMhybExhaustive(phy.graph=nicotiana$phy.graph, traits=nicotiana$trait, confidence.points=5000)
+n.results <- BMhybExhaustive(phy.graph=nicotiana$phy.graph, traits=nicotiana$trait, confidence.points=5000, ncores=8)
 save(n.results, file="NicotianaResults.rda")
