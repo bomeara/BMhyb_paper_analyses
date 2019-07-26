b9ecdad1c10945b5f43d020c2eade14267276a40
Brian O'Meara <omeara.brian@gmail.com>
2019-07-26 15:48:02 -0400

    updating plot code

--- condor_approach/plot.R
+++ condor_approach/plot.R
@@ -19,38 +19,38 @@ plotresults <- function(results) {
     
     
     sigma.sq.plot <- ggplot(results, aes(x=Number_of_taxa, y=sigma.sq, fill=Number_of_hybridizations)) +
-        geom_violin() + geom_hline(yintercept=unique(results$sigma.sq.true)) + scale_fill_viridis_d() 
+        geom_boxplot(notch=TRUE) + geom_hline(yintercept=unique(results$sigma.sq.true)) + scale_fill_viridis_d() 
     print(sigma.sq.plot)
     
     mu.plot <- ggplot(results, aes(x=Number_of_taxa, y=mu, fill=Number_of_hybridizations)) +
-        geom_violin() + geom_hline(yintercept=unique(results$mu.true)) + scale_fill_viridis_d()
+        geom_boxplot(notch=TRUE) + geom_hline(yintercept=unique(results$mu.true)) + scale_fill_viridis_d()
     print(mu.plot)
     
     vh.plot <- ggplot(results, aes(x=Number_of_taxa, y=vh, fill=Number_of_hybridizations)) +
-        geom_violin() + geom_hline(data = results, aes(yintercept = vh.true)) +  facet_wrap(~ Number_of_hybridizations + True_vh, nrow=1, labeller = label_both) + scale_fill_viridis_d()
+        geom_boxplot(notch=TRUE) + geom_hline(data = results, aes(yintercept = vh.true)) +  facet_wrap(~ Number_of_hybridizations + True_vh, nrow=1, labeller = label_both) + scale_fill_viridis_d()
     print(vh.plot)
   
     
     SE.plot <- ggplot(results, aes(x=Number_of_taxa, y=SE, fill=Number_of_hybridizations)) +
-        geom_violin() + geom_hline(data = results, aes(yintercept = SE.true)) +  facet_wrap(~ Number_of_hybridizations + True_SE, nrow=1, labeller = label_both) + scale_fill_viridis_d()
+        geom_boxplot(notch=TRUE) + geom_hline(data = results, aes(yintercept = SE.true)) +  facet_wrap(~ Number_of_hybridizations + True_SE, nrow=1, labeller = label_both) + scale_fill_viridis_d()
     print(SE.plot)
     
     
     bt.plot <- ggplot(results, aes(x=Number_of_taxa, y=bt, fill=True_beta)) +
-        geom_violin() + facet_wrap(~ Number_of_hybridizations + True_beta, nrow=1, labeller = label_both) + geom_hline(data = results, aes(yintercept = bt.true)) + scale_fill_viridis_d()
+        geom_boxplot(notch=TRUE) + facet_wrap(~ Number_of_hybridizations + True_beta, nrow=1, labeller = label_both) + geom_hline(data = results, aes(yintercept = bt.true)) + scale_fill_viridis_d()
     print(bt.plot)
     
     bt.plot2 <- ggplot(results, aes(x=Number_of_taxa, y=Weight_for_free_beta, fill=True_beta)) +
-        geom_violin() + facet_wrap(~ Number_of_hybridizations + True_beta, nrow=1, labeller = label_both) + scale_fill_viridis_d()
+        geom_boxplot(notch=TRUE) + facet_wrap(~ Number_of_hybridizations + True_beta, nrow=1, labeller = label_both) + scale_fill_viridis_d()
     print(bt.plot2)
     
     vh.plot2 <- ggplot(results, aes(x=Number_of_taxa, y=Weight_for_free_vh, fill=True_vh)) +
-        geom_violin() + facet_wrap(~ Number_of_hybridizations + True_vh, nrow=1, labeller = label_both) + scale_fill_viridis_d()
+        geom_boxplot(notch=TRUE) + facet_wrap(~ Number_of_hybridizations + True_vh, nrow=1, labeller = label_both) + scale_fill_viridis_d()
     print(vh.plot2)
     
     
     SE.plot2 <- ggplot(results, aes(x=Number_of_taxa, y=Weight_for_free_SE, fill=True_SE)) +
-        geom_violin() + facet_wrap(~ Number_of_hybridizations + True_SE, nrow=1, labeller = label_both) + scale_fill_viridis_d()
+        geom_boxplot(notch=TRUE) + facet_wrap(~ Number_of_hybridizations + True_SE, nrow=1, labeller = label_both) + scale_fill_viridis_d()
     print(SE.plot2)
     
     if(any(grepl("ModelType", colnames(results)))) {
