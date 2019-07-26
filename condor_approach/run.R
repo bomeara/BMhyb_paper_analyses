fd8829b51d937fd9becc1386dbc38589a42de426
Brian O'Meara <omeara.brian@gmail.com>
2019-07-26 15:51:56 -0400

    updating for new version of BMhyb

--- condor_approach/run.R
+++ condor_approach/run.R
@@ -2,10 +2,9 @@ args <- commandArgs(trailingOnly = TRUE)
 print("args")
 print(args)
 
-setSessionTimeLimit(cpu=24*60*60) # stop a job after 12 hours: could be stalled
+#setSessionTimeLimit(cpu=24*60*60) # stop a job after 12 hours: could be stalled
 
-
-condition.matrix <- expand.grid(ntax=c(30,100), nhybridizations=c(1,5,10), tree.height=50, sigma.sq=0.01, mu=1, bt=c(1,2), vh=c(0,0.1*50*0.01), SE=c(0, 0.1*sqrt(0.01*50)), gamma=0.5, rep=sequence(25)) #for Vh, zero, as much variance as you get from 10% of evo history, or as much variance as you get from the whole tree. For SE, 10% and half of the variance from BM
+condition.matrix <- expand.grid(ntax=c(30,100), nhybridizations=c(1,5,10), tree.height=50, sigma.sq=0.01, mu=1, bt=c(1,2), vh=c(0, 0.1*50*0.01, 0.5*50*0.01), SE=c(0, 0.1*sqrt(0.01*50)), gamma=0.5, rep=sequence(25)) #for Vh, zero, as much variance as you get from 10% of evo history, or as much variance as you get from 50% of the whole tree. For SE, 10% and half of the variance from BM
 
 base_number <- as.numeric(args[2]) + 1
 
@@ -36,4 +35,6 @@ DoRunTemplated <- function(ntax, nhybridizations, tree.height, sigma.sq, mu, bt,
 
 results <- DoRunTemplated(ntax=conditions$ntax, nhybridizations=conditions$nhybridizations, tree.height=conditions$tree.height, sigma.sq=conditions$sigma.sq, mu=conditions$mu, bt=conditions$bt, vh=conditions$vh, SE=conditions$SE, gamma=conditions$gamma)
 
-save(results, conditions, file=paste0("Result_arg1_", args[1], "_arg2_", args[2], "_ntax_", conditions$ntax, "_nhybridizations_",conditions$nhybridizations, "_treeheight_",conditions$tree.height, "_sigmasq_",conditions$sigma.sq, "_mu_",conditions$mu, "_bt_", conditions$bt, "_vh_", conditions$vh, "_SE_", round(conditions$SE,5), "_gamma_", conditions$gamma,"_rep_", conditions$rep, ".rda"))
+saved_session_info <- sessionInfo()
+
+save(results, conditions, saved_session_info, file=paste0("Result_arg1_", args[1], "_arg2_", args[2], "_ntax_", conditions$ntax, "_nhybridizations_",conditions$nhybridizations, "_treeheight_",conditions$tree.height, "_sigmasq_",conditions$sigma.sq, "_mu_",conditions$mu, "_bt_", conditions$bt, "_vh_", conditions$vh, "_SE_", round(conditions$SE,5), "_gamma_", conditions$gamma,"_rep_", conditions$rep, ".rda"))
