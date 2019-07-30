49ab5675dd87e67f23328209d8626a712f0316c1
Brian O'Meara <omeara.brian@gmail.com>
2019-07-30 11:17:34 -0400

    better plotting

--- empirical/gather.R
+++ empirical/gather.R
@@ -52,17 +52,19 @@ for (param_index in seq_along(params)) {
   print(weighted.mean(c.results[,params[param_index]], c.results$AkaikeWeight))
 }
 
-graphics::par(mfcol=c(1, length(params)))
-for(parameter in sequence(length(params))) {
-    graphics::plot(x=all.sims[,parameter+1], y=all.sims[,1], type="n", xlab=params[parameter], ylab="NegLnL", bty="n", ylim=c(min(all.sims[,1]), max(all.sims[,1])))
-    graphics::points(x=x$good.region[,parameter+1], y=x$good.region[,1], pch=16, col="black")
-    graphics::points(x=x$bad.region[,parameter+1], y=x$bad.region[,1], pch=16, col="gray")
-    #graphics::points(x= x$best[parameter], y= x$best['NegLogLik'], pch=1, col=focal.color, cex=1.5)
-}
+# graphics::par(mfcol=c(1, length(params)))
+# for(parameter in sequence(length(params))) {
+#     graphics::plot(x=all.sims[,parameter+1], y=all.sims[,1], type="n", xlab=params[parameter], ylab="NegLnL", bty="n", ylim=c(min(all.sims[,1]), max(all.sims[,1])))
+#     graphics::points(x=x$good.region[,parameter+1], y=x$good.region[,1], pch=16, col="black")
+#     graphics::points(x=x$bad.region[,parameter+1], y=x$bad.region[,1], pch=16, col="gray")
+#     #graphics::points(x= x$best[parameter], y= x$best['NegLogLik'], pch=1, col=focal.color, cex=1.5)
+# }
 
 load("NicotianaResults.rda")
 class(n.results) <- "BMhybExhaustiveResult"
-#merged <- MergeExhaustiveForPlotting(n.results)
-#plot(merged, style="contour")
-plot(n.results)
+pdf(file="NicotianaResults.pdf")
+print(plot(n.results, style="contour", nrow=2))
+dev.off()
+
+ggplot2::ggsave("NicotianaResults_ggsave.pdf")
 summary(n.results)
